3f65fd42ae136f1575e4d33dac78ea7f57f3410b
Dario Camonita <danterolle@catania.linux.it>
2023-02-21 20:30:06 +0100

    Added iterazione1 refactoring project

---
+++ Elaborazione/Iterazione 1/project/iterazione1refactoring/app.py
@@ -0,0 +1,104 @@
+import os
+import qrcode
+import random
+
+
+class Cliente:
+    def __init__(self, nome, cognome, email, cellulare, idTavolo):
+        self.nome = nome
+        self.cognome = cognome
+        self.email = email
+        self.cellulare = cellulare
+        self.idTavolo = idTavolo
+
+
+class Prenotazione:
+    def __init__(self, id, num_persone, data, ora):
+        self.id = id
+        self.num_persone = num_persone
+        self.data = data
+        self.ora = ora
+
+
+class SUR:
+    __instance = None
+
+    @staticmethod
+    def getInstance():
+        if SUR.__instance is None:
+            SUR()
+        return SUR.__instance
+
+    def __init__(self):
+        if SUR.__instance is not None:
+            raise Exception("Pattern Singleton!")
+        else:
+            SUR.__instance = self
+        self.prenotazioni = []
+
+    def inserimentoPrenotazione(self, nome, cognome, email, cellulare, num_persone, data, ora, idTavolo):
+        cliente = Cliente(nome, cognome, email, cellulare, idTavolo)
+        prenotazione = Prenotazione(len(self.prenotazioni) + 1, num_persone, data, ora)
+        self.prenotazioni.append((cliente, prenotazione))
+
+    def confermaInserimento(self):
+        print("Prenotazione confermata!")
+        cliente, prenotazione = self.prenotazioni[-1]
+        print("Prenotazione:")
+        print(f"- ID: {prenotazione.id}")
+        print(f"- Data: {prenotazione.data}")
+        print(f"- Ora: {prenotazione.ora}")
+        print(f"- Numero di persone: {prenotazione.num_persone}")
+        print("Cliente:")
+        print(f"- Nome: {cliente.nome}")
+        print(f"- Cognome: {cliente.cognome}")
+        print(f"- Email: {cliente.email}")
+        print(f"- Cellulare: {cliente.cellulare}")
+        print(f"- ID Tavolo: {cliente.idTavolo}")
+
+    def generaCodiceQR(self, file_path):
+        if not self.prenotazioni:
+            raise ValueError("Non ci sono prenotazioni da salvare.")
+        cliente, prenotazione = self.prenotazioni[-1]
+        qr_data = f"Prenotazione:\nID: {prenotazione.id}\nData: {prenotazione.data}\nOra: {prenotazione.ora}\nNumero di persone: {prenotazione.num_persone}\nCliente:\nNome: {cliente.nome}\nCognome: {cliente.cognome}\nEmail: {cliente.email}\nCellulare: {cliente.cellulare}\nID Tavolo: {cliente.idTavolo}"
+        img = qrcode.make(qr_data)
+        dir_path = "./qrcodes"
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        file_path = os.path.join(dir_path, f"prenotazione_{prenotazione.id}.png")
+        img.save(file_path)
+        print(f"Codice QR salvato in {file_path}")
+
+
+def main():
+    sur = SUR.getInstance()
+    while True:
+        print("\nBenvenuto in Speedup Restaurant!")
+        print("Cosa vuoi fare?\n")
+        print("1. Effettuare una nuova prenotazione")
+        print("q. Uscire")
+        scelta = input("Scelta: ")
+        if scelta == "1":
+            nome = input("Inserisci il tuo nome: ")
+            cognome = input("Inserisci il tuo cognome: ")
+            email = input("Inserisci la tua email: ")
+            cellulare = input("Inserisci il tuo numero di cellulare: ")
+            num_persone = int(input("Inserisci il numero di persone: "))
+            data = input("Inserisci la data (YYYY-MM-DD): ")
+            ora = input("Inserisci l'ora (HH:MM): ")
+            idTavolo = random.randint(1, 20)
+            sur.inserimentoPrenotazione(nome, cognome, email, cellulare, num_persone, data, ora, idTavolo)
+            sur.confermaInserimento()
+            sur.generaCodiceQR()
+            # Dal 2022 Gmail, Outlook e altri servizi email non consentono l'invio di email da client insicuri come
+            # questo.
+            print("Email inviata!")
+        if scelta == "q":
+            print("Grazie per aver usato Speedup Restaurant!")
+            break
+        else:
+            print("input non valido, scegliere una delle opzioni precedenti.")
+
+
+if __name__ == "__main__":
+    main()
